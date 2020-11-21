f9d9178b9e71e4f62edd1aee276cd774036645b5
Matt Harris <711mattman@gmail.com>
2020-11-20 22:41:33 -0500

    Adjusted the placement of the script tags in the html

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express ();
 
 const PORT = process.env.PORT || 3000;
 
-const headDir = path.join(_dirname, "/public");
+const headDir = path.join(__dirname, "/public");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
