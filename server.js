af0c6939c133533f8b7d37d054bafd09ce06faa5
Matt Harris <711mattman@gmail.com>
2020-11-20 22:19:05 -0500

    The routes option is not working for me going to try something else

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require ("express");
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js");
+
 
 
 const app = express ();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(express.json());
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+require("../routes/apiRoutes")(app);
+require("../routes/htmlRoutes")(app);
 
-app.listen(PORT, () => console.log(`Listen on PORT: ${PORT}`));
+
+app.listen(PORT, () => console.log("Listen on PORT: " + PORT));
