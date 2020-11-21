d2c6dcd45cb810b2bcd947557d56df4ba6829cb8
Matt Harris <711mattman@gmail.com>
2020-11-21 10:57:12 -0500

    I am trying to get this to work, but the css won't load if I do it one way, and the notes.html takes forver to load this way

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,11 @@
 const path = require("path");
-
-module.exports = function(app) {
-
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-}
+const router = require("express").Router();
+// "/notes" responds with the notes.html file
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+// All other routes respond with the index.html file
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+module.exports = router;
